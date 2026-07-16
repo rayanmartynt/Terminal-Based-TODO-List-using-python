f2c885bcf31f8f289a56565b84c3b5b4badc7929
rayanmartynt <rayanmartynt@gmail.com>
2026-07-16 22:18:51 +0000

    Adding remove task function

--- main.py
+++ main.py
@@ -11,8 +11,19 @@ def show_task():
         for i in range(len(tasks)):
             print(f"{i + 1}.  {tasks[i]}")
 
+# Function 2: Add task
 def add_task():
     task = input("Enter a new task.")
     tasks.append(task)
     print("Task added successfully.")
     show_task()
+
+# Function 3: Remove task
+def remove_task():
+    if len(tsks) > 0:
+        number = int(input("Enter the number of the task you want to remove:"))
+        if 1 <= number <= len(tasks):
+            removed = tasks.pop(number - 1)
+            print(f"{removed} removed")
+        else:
+            print("Invalid task number.")
